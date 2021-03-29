da0be6235fb84e10dff6199cb5862df757f987d9
John Cigale <jcigale98@gmail.com>
2021-03-29 15:51:46 -0400

    event schema

---
+++ models/Event.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: Text,
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Tweet = mongoose.model('event', EventSchema);
+module.exports = Tweet;
