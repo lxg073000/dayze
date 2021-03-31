10cc059a3a9811c7b27399c6876567bc48635b28
John Cigale <jcigale98@gmail.com>
2021-03-31 17:05:33 -0400

    frontend build

--- app.js
+++ app.js
@@ -35,13 +35,12 @@ app.use("/api/events", events);
 
 app.use("/api/users", users);
 
-
-app.use(express.static('frontend/build'));
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-})
-
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
