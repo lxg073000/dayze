9498aea1c1331f7560c2c0313b2df88a09e616c4
John Cigale <jcigale98@gmail.com>
2021-03-31 16:14:48 -0400

    keys for heroku

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dayze:E8M9iHqCZAXpwTYg@cluster0.zgurr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: 'abettersecret1029384756'
+}
