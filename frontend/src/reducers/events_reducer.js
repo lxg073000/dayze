9c12bfe02f93bab59301e7be0b33572cab7ab60f
John Cigale <jcigale98@gmail.com>
2021-03-31 15:57:34 -0400

    break to push to heroku

--- frontend/src/reducers/events_reducer.js
+++ frontend/src/reducers/events_reducer.js
@@ -11,6 +11,14 @@ const EventsReducer = (state = { all: {}, user: {}, new: undefined }, action) =>
     Object.freeze(state);
     let newState = Object.assign({}, state);
     switch(action.type) {
-        
+        case RECEIVE_EVENTS:
+            newState.all = action.events.data;
+            return newState;
+        case RECEIVE_USER_EVENTS:
+            newState.user = action.events.data;
+            return newState;
+        case RECEIVE_NEW_EVENT:
+            newState.new = action.events.data;
+            return newState;
     }
 }
