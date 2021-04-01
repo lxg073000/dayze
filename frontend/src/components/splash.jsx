37c35f715e76e7fda6605329641e10790827a17b
App Academy Student <lxg073000@gmail.com>
2021-04-01 14:25:46 -0400

    remove old files

--- frontend/src/components/splash.jsx
+++ frontend/src/components/splash.jsx
@@ -3,31 +3,27 @@ import { Link } from "react-router-dom";
 
 export default function splash() {
   return (
-    <div>
-      <div>
-        <div className="blurry">
-          <div className="splashfill"></div>
+    <div className="splash-page-container">
+      <div className="blurry">
+        <div className="splashfill"></div>
+      </div>
+      <div className="splash-contents">
+        <div className="logo">
+          <p className="logo-text">Dayze</p>
+          <p className="tagline">we'll remember when.</p>
         </div>
-        <div className="page-container">
-          <nav className="nav"></nav>
-          <div className="banner">
-            <p className="logo-text">Dayze</p>
-            <p className="tagline">we'll remember when.</p>
-          </div>
-          <div className="blurry"></div>
-          <footer className="splash-nav">
-            <ul className="nav-items">
-              <li>
-                <Link to="./login">Log In</Link>
-              </li>
-              <li>
-                <Link to="./signup">Sign Up</Link>
-              </li>
-              <li>
-                <Link to="./user">Continue As Guest</Link>
-              </li>
-            </ul>
-          </footer>
+        <div className="session-nav">
+          <ul className="nav-items">
+            <li>
+              <Link to="./login">Log In</Link>
+            </li>
+            <li>
+              <Link to="./signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="./user">Continue As Guest</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
