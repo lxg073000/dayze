4eb1708b93951c4240e197ba2c400ccfcfa11186
App Academy Student <lxg073000@gmail.com>
2021-04-16 12:37:12 -0400

    remove debgger

--- util/session_api_util.js
+++ util/session_api_util.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000"
+axios.defaults.baseURL = "http://localhost:5000";
 
-export const setAuthToken = token => {
+export const setAuthToken = (token) => {
   if (token) {
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common["Authorization"] = token;
   } else {
-    delete axios.defaults.headers.common['Authorization'];
+    delete axios.defaults.headers.common["Authorization"];
   }
 };
 
 export const signup = (userData) => {
-  debugger
-  console.log(axios.baseURL)
-  return axios.post('/api/users/register', userData);
+  // debugger
+  console.log(axios.baseURL);
+  return axios.post("/api/users/register", userData);
 };
 
 export const login = (userData) => {
-  return axios.post('/api/users/login', userData);
+  return axios.post("/api/users/login", userData);
 };
