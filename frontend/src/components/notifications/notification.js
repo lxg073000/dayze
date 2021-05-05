6fb051d52011b2a24ce03406eeb5130ae560003c
App Academy Student <lxg073000@gmail.com>
2021-05-05 17:30:37 -0400

    stub notification showpage

--- frontend/src/components/notifications/notification.js
+++ frontend/src/components/notifications/notification.js
@@ -3,13 +3,18 @@ import React from "react";
 export default function notification() {
   let urlParams = new URLSearchParams(window.location.search);
   let [desc, title, date] = urlParams.values();
-  
+
   return (
     <div className="notifcations-shell">
       Notification.
+      <img
+        className="notification-bg"
+        alt="alarm-bg"
+        src="https://images.unsplash.com/photo-1501622549218-2c3ef86627cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+      ></img>
       <h1>{title}</h1>
       <h2>{date}</h2>
       <p>{desc}</p>
     </div>
-  )
+  );
 }
