1d888ed061f9adbefcdb79308f56b08761ebb33f
Ray <raysidarta@gmail.com>
2021-05-06 12:06:03 -0400

    Change link to google authentication page to link to dayze github wiki in  googleUrl component

--- frontend/src/components/session/google_url.js
+++ frontend/src/components/session/google_url.js
@@ -1,4 +1,18 @@
+/*  
+* The Dayze app is able to use google calendar api to sync itself with a user's
+calendar.  The use of reading and writing another person's Google calendar 
+requires app verification from Google.  Due to time constraints, we are able to 
+only sync calendars for authorized test users.
+
+Because of this, in the code below, the link to the Google authentication page 
+is replaced with a link to the Dayze github wiki.
+*/
+
+
+
+import { calendar } from "googleapis/build/src/apis/calendar";
 import React, { Component } from "react";
+import { useStore } from "react-redux"
 
 export default class googleUrl extends Component {
   constructor(props) {
@@ -10,8 +24,6 @@ export default class googleUrl extends Component {
         token3: null,
       },
     };
-
-    // this.handleTokens = this.handleTokens.bind(this);
   }
 
 
@@ -32,7 +44,8 @@ export default class googleUrl extends Component {
           <div className="google-URL-grid">
             <a
               className="sync-confirm-btn"
-              href={this.props.currentUser.googleUrl}
+              href = {'https://github.com/lxg073000/dayze/wiki'}
+              // href={this.props.currentUser.googleUrl}     //See above * for comments.
               onClick={() =>
                 document
                   .getElementsByClassName("oauth-modal-container")[0]
